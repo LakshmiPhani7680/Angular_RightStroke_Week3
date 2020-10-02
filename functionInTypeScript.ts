d9e63e7e11aa80817539f05ee2fdbef2cb4c8e2d
LakshmiPhani7680 <48641068+LakshmiPhani7680@users.noreply.github.com>
2020-10-02 16:44:05 +0530

    functionInTypeScript
    Develop a program that will exhibit functions as :- a) Function with default parameter, b)Function with optional parameter, c) Function with Rest Parameter

---
+++ functionInTypeScript.ts
@@ -0,0 +1,25 @@
+//Function with default parameters
+function Attendance(name: string, presence: string = "Present") : string {
+    return name +" is " +presence + ' ' + '!';
+}
+
+console.log(Attendance('ravi'));
+console.log(Attendance('pavan', 'Present')); 
+console.log(Attendance('sanjay')); 
+
+//Functions with  optinal parameters
+function Attend(presense: string, name?: string ) : string {
+    return  name+" is "+presense + "!";
+}
+
+console.log(Attend('Present','Steve'));
+console.log(Attend('Present')); 
+
+//Function with rest parameters
+function Attendies(presense: string, ...names: string[]) {
+    return  names.join(", ")+"are "+presense + "!";
+}
+
+console.log(Attendies("Present", "Srinu", "Pavan"));
+
+console.log(Attendies("Present"));
